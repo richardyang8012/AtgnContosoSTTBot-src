1d2f331a1b406b74d45094b0cd73095824573b0a
richardyang8012 <37422644+richardyang8012@users.noreply.github.com>
2018-04-23 10:55:33 -0700

    Initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=atgncontososttbot-Web-Deploy.pubxml -p:Password=Sz796rxeZk47pilfPglgvE0zBeXjQe3wNyp1nFJ6x4ju9tgfvjupftpEovDF
+
